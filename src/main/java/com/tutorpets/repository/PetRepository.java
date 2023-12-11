941542546fba894a36c1709840793319004a9f06
Tiago <tiagaoalb@gmail.com>
2023-12-11 18:34:11 -0300

    chore: add PetRepository

---
+++ src/main/java/com/tutorpets/repository/PetRepository.java
@@ -0,0 +1,13 @@
+package com.tutorpets.repository;
+
+import com.tutorpets.model.Pet;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PetRepository extends JpaRepository<Pet, Long> {
+
+    List<Pet> findPetByNameContainingIgnoreCase(String name);
+}
