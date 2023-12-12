3e865e4504aefd04b1c3ee974d809eab9cb54dd4
Tiago Bovolin <tiagaoalb@gmail.com>
2023-12-11 22:53:29 -0300

    chore: add PetDTOMapper

---
+++ src/main/java/com/tutorpets/model/dto/PetDTOMapper.java
@@ -0,0 +1,23 @@
+package com.tutorpets.model.dto;
+
+import com.tutorpets.model.Pet;
+import org.springframework.stereotype.Service;
+
+import java.util.function.Function;
+
+@Service
+public class PetDTOMapper implements Function<Pet, PetDTO> {
+    @Override
+    public PetDTO apply(Pet pet) {
+        return new PetDTO(
+                pet.getId(),
+                pet.getName(),
+                pet.getPetBreed(),
+                pet.getBirthDate(),
+                pet.getColor(),
+                pet.getWeight(),
+                pet.getVaccineDate(),
+                pet.getVaccineType()
+        );
+    }
+}
