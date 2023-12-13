fefbad2dcea530e22c9145190b3ab1dae141a92b
Tiago <tiagaoalb@gmail.com>
2023-12-13 15:13:30 -0300

    chore: create pet using dto

--- src/main/java/com/tutorpets/service/PetService.java
+++ src/main/java/com/tutorpets/service/PetService.java
@@ -1,13 +1,14 @@
 package com.tutorpets.service;
 
 import com.tutorpets.model.Pet;
+import com.tutorpets.model.Tutor;
 import com.tutorpets.model.dto.PetDTO;
 import com.tutorpets.model.dto.PetDTOMapper;
 import com.tutorpets.repository.PetRepository;
+import com.tutorpets.repository.TutorRepository;
 import com.tutorpets.service.exception.DataNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -15,25 +16,37 @@ import java.util.List;
 public class PetService {
 
     private final PetRepository petRepository;
+    private final TutorRepository tutorRepository;
     private final PetDTOMapper petDTOMapper;
 
-    public PetService(PetRepository petRepository, PetDTOMapper petDTOMapper) {
+    public PetService(PetRepository petRepository, TutorRepository tutorRepository, PetDTOMapper petDTOMapper) {
         this.petRepository = petRepository;
+        this.tutorRepository = tutorRepository;
         this.petDTOMapper = petDTOMapper;
     }
 
-    public Pet addPet(Pet pet) {
+    public Pet addPet(PetDTO petDTO) {
+        Pet pet = new Pet();
+        pet.setName(petDTO.name());
+        pet.setPetBreed(petDTO.petBreed());
+        pet.setBirthDate(petDTO.birthDate());
+        pet.setColor(petDTO.color());
+        pet.setWeight(petDTO.weight());
+        pet.setVaccineDate(petDTO.vaccineDate());
+        pet.setVaccineType(petDTO.vaccineType());
+
+        Tutor tutor = tutorRepository.findTutorByName(petDTO.tutorName());
+
+        pet.setTutor(tutor);
         return petRepository.save(pet);
     }
 
-    @Transactional
     public List<PetDTO> findAllPets() {
         return petRepository.findAll()
                 .stream()
                 .map(petDTOMapper).toList();
     }
 
-    @Transactional
     public PetDTO findPetById(Long id) {
         return petRepository.findById(id)
                 .map(petDTOMapper)
