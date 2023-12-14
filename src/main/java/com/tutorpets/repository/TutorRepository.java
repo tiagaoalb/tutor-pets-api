f5b1cc3dd8a845b49044984cf656916e301269ae
Tiago <tiagaoalb@gmail.com>
2023-12-14 19:26:38 -0300

    chore: create custom queries to find tutors

--- src/main/java/com/tutorpets/repository/TutorRepository.java
+++ src/main/java/com/tutorpets/repository/TutorRepository.java
@@ -2,6 +2,8 @@ package com.tutorpets.repository;
 
 import com.tutorpets.model.Tutor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,5 +13,26 @@ public interface TutorRepository extends JpaRepository<Tutor, Long> {
 
     Tutor findTutorByName(String tutorName);
 
-    List<Tutor> findTutorByNameContainingIgnoreCase(String name);
+    @Query("""
+            select t
+            from Tutor t
+            left join fetch t.pets p
+            where lower(t.name) like lower(concat('%', :name, '%'))
+            """)
+    List<Tutor> findTutorByNameContainingIgnoreCase(@Param("name") String name);
+
+    @Query("""
+            select t
+            from Tutor t
+            left join fetch t.pets p
+            """)
+    List<Tutor> findAllTutors();
+
+    @Query("""
+            select t
+            from Tutor t
+            left join fetch t.pets p
+            where t.id = :tutorId
+            """)
+    Tutor findTutorById(@Param("tutorId") Long tutorId);
 }
