f078fdcde7f2194d2ef1e753a3fa09a46aef101c
Tiago <tiagaoalb@gmail.com>
2023-12-15 11:46:46 -0300

    chore: add exception on findById method

--- src/main/java/com/tutorpets/service/TutorService.java
+++ src/main/java/com/tutorpets/service/TutorService.java
@@ -4,6 +4,8 @@ import com.tutorpets.model.Tutor;
 import com.tutorpets.model.dto.TutorDTO;
 import com.tutorpets.model.dto.TutorDTOMapper;
 import com.tutorpets.repository.TutorRepository;
+import com.tutorpets.service.exception.DataNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -38,7 +40,9 @@ public class TutorService {
 
     public Optional<TutorDTO> findTutorById(Long id) {
         Optional<Tutor> tutorOptional = Optional.ofNullable(tutorRepository.findTutorById(id));
-        return tutorOptional.map(tutorDTOMapper);
+        return Optional.ofNullable(tutorOptional.map(tutorDTOMapper)
+                .orElseThrow(
+                        () -> new DataNotFoundException(HttpStatus.NOT_FOUND, "Pet not found with id" + id)));
     }
 
     public List<TutorDTO> findTutorByName(String name) {
