d45c9c7e2a367283cbc87e404470e18cb4f32eec
Tiago Bovolin <tiagaoalb@gmail.com>
2023-12-15 21:19:30 -0300

    chore: add TutorServiceTest file

---
+++ src/test/java/com/tutorpets/service/TutorServiceTest.java
@@ -0,0 +1,24 @@
+package com.tutorpets.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class TutorServiceTest {
+
+    @Test
+    @DisplayName("Tutor should be saved")
+    void addTutor_ShouldAddTutor() {
+    }
+
+    @Test
+    void findAllTutors() {
+    }
+
+    @Test
+    void findTutorById() {
+    }
+
+    @Test
+    void findTutorByName() {
+    }
+}
