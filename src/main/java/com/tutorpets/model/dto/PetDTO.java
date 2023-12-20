35656b8465b70c4a30d58a00a93b70a976ffc794
Tiago <tiagaoalb@gmail.com>
2023-12-20 18:29:12 -0300

    chore: add validation annotations

--- src/main/java/com/tutorpets/model/dto/PetDTO.java
+++ src/main/java/com/tutorpets/model/dto/PetDTO.java
@@ -1,16 +1,35 @@
 package com.tutorpets.model.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
 import java.time.LocalDate;
 
 public record PetDTO(
         Long id,
+        @NotNull
+        @NotEmpty
         String name,
+        @NotNull
+        @NotEmpty
         String petBreed,
+        @NotNull
+        @NotEmpty
         LocalDate birthDate,
+        @NotNull
+        @NotEmpty
         String color,
+        @NotNull
+        @NotEmpty
         Double weight,
+        @NotNull
+        @NotEmpty
         LocalDate vaccineDate,
+        @NotNull
+        @NotEmpty
         String vaccineType,
+        @NotNull
+        @NotEmpty
         String tutorName
 ) {
 }
--- src/main/java/com/tutorpets/model/dto/TutorDTO.java
+++ src/main/java/com/tutorpets/model/dto/TutorDTO.java
@@ -1,13 +1,24 @@
 package com.tutorpets.model.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
 import java.time.LocalDate;
 import java.util.List;
 
 public record TutorDTO (
         Long id,
+        @NotNull
+        @NotEmpty
         String name,
+        @NotNull
+        @NotEmpty
         String nickName,
+        @NotNull
+        @NotEmpty
         LocalDate birthDate,
+        @NotNull
+        @NotEmpty
         List<PetDTO> pets
 ) {
 }
