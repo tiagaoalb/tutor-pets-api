1441011ff699002caa7f166bd4ccbb5d78b08965
Tiago Bovolin <tiagaoalb@gmail.com>
2023-12-21 23:55:12 -0300

    chore: add assertions on main test

--- src/test/java/com/tutorpets/TutorPetsApiApplicationTests.java
+++ src/test/java/com/tutorpets/TutorPetsApiApplicationTests.java
@@ -1,13 +1,24 @@
 package com.tutorpets;
 
+import com.tutorpets.controller.PetController;
+import com.tutorpets.controller.TutorController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class TutorPetsApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private TutorController tutorController;
+	@Autowired
+	private PetController petController;
 
+	@Test
+	void contextLoads()throws Exception {
+		assertThat(tutorController).isNotNull();
+		assertThat(petController).isNotNull();
+	}
 }
